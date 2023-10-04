12c0c26e7766b24203cbc478a69777157fe955d3
Carolina Lara <carolara140@gmail.com>
2023-10-03 22:36:43 -0600

    Translation in prgoress

--- Proteins.py
+++ Proteins.py
@@ -1,7 +1,178 @@
 
+codon_dict = {
+        "MGY": "Met",
+        "INV": "Ile",
+        "FAA": "Phe",
+        "FPT": "Phe",
+        "IYS": "Tyr",
+        "LLL": "Leu",
+        "CRM": "Cys",
+        "NS": "Asn",
+        "RNY": "Arg",
+        "IAQ": "Ile",
+        "VDV": "Val",
+        "VNF": "Val",
+        "NLT": "Leu"
+    }
 
-# 3.Dado el archivo de las proteínas, el cual contiene sus secuencias de aminoácidos, encuentra en cuales secciones del virus se produce cada proteína.
-# Muestra el nombre de la proteína y los índices del genoma del virus que la producen. Muestra sus primeros 4 aminoácidos, y codones asociados del virus.
+aminoacido_dict = {
+        "Ala": ["GCT", "GCC", "GCA", "GCG"],
+        "A": ["GCT", "GCC", "GCA", "GCG"],
+        "Arg": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],
+        "R": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],
+        "Asn": ["AAT", "AAC"],
+        "N": ["AAT", "AAC"],
+        "Asp": ["GAT", "GAC"],
+        "D": ["GAT", "GAC"],
+        "Cys": ["TGT", "TGC"],
+        "C": ["TGT", "TGC"],
+        "Gln": ["CAA", "CAG"],
+        "Q": ["CAA", "CAG"],
+        "Glu": ["GAA", "GAG"],
+        "E": ["GAA", "GAG"],
+        "Gin": ["CAA", "CAG"],
+        "Z": ["CAA", "CAG"],
+        "Gly": ["GGT", "GGC", "GGA", "GGG"],
+        "G": ["GGT", "GGC", "GGA", "GGG"],
+        "His": ["CAT", "CAC"],
+        "H": ["CAT", "CAC"],
+        "Ile": ["ATT", "ATC", "ATA"],
+        "I": ["ATT", "ATC", "ATA"],
+        "START": ["ATG"],
+        "lle": ["ATT", "ATC", "ATA"],
+        "Leu": ["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"],
+        "L": ["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"],
+        "Lys": ["AAA", "AAG"],
+        "K": ["AAA", "AAG"],
+        "Met": ["ATG"],
+        "M": ["ATG"],
+        "Phe": ["TTT", "TTC"],
+        "F": ["TTT", "TTC"],
+        "Pro": ["CCT", "CCC", "CCA", "CCG"],
+        "P": ["CCT", "CCC", "CCA", "CCG"],
+        "Ser": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+        "S": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+        "Thr": ["ACT", "ACC", "ACA", "ACG"],
+        "T": ["ACT", "ACC", "ACA", "ACG"],
+        "Trp": ["TGG"],
+        "W": ["TGG"],
+        "Tyr": ["TAT", "TAC"],
+        "Y": ["TAT", "TAC"],
+        "Val": ["GTT", "GTC", "GTA", "GTG"],
+        "V": ["GTT", "GTC", "GTA", "GTG"],
+        "STOP": ["TAA", "TAG", "TGA"],
+    }
 
-# 4. Compara las versiones del genoma del virus de Wuhan, 2019 vs Texas, 2020. Determina donde  difieren, y si tales diferencias resultan en aminoácidos diferentes.
-# Lista los índices donde encuentres diferencias, muestra los codones afectados por tal diferencia y el aminoácido producido por cada versión.
+
+def traducir_proteina_a_nucleotidos(cadena_proteina):
+    codones_proteina = []
+    # Traducir la cadena de proteínas al diccionario de codones
+    for i in range(0, len(cadena_proteina), 3):
+        codon = cadena_proteina[i:i + 3]
+        if codon in codon_dict:
+            codones_proteina.append(codon_dict[codon])
+        else:
+            raise ValueError(f"El codón {codon} no se encuentra en el diccionario.")
+
+        nucleotidos = []
+        # Traducir los aminoácidos del diccionario de aminoácidos a codones
+        for aminoacido in codones_proteina:
+            if aminoacido in aminoacido_dict:
+                nucleotidos.extend(aminoacido_dict[aminoacido])
+            else:
+                raise ValueError(f"El aminoácido {aminoacido} no se encuentra en el diccionario de aminoácidos.")
+
+        return ''.join(nucleotidos)
+
+    # Ejemplo de uso
+
+cadena_proteina = "MGYINVFAFPFTIYSLLLCRMNSRNYIAQVDVVNFNLT"
+print("Secuencia de proteína:")
+cadena_nucleotidos = traducir_proteina_a_nucleotidos(cadena_proteina)
+print("Secuencia de nucleótidos resultante:")
+print(cadena_nucleotidos)
+import string
+
+
+codon_dict = {
+    "MGY": "Met",
+    "INV": "Ile",
+    "FAF": "Phe",
+    "PFT": "Phe",
+    "IYS": "Tyr",
+    "LLL": "Leu",
+    "CRM": "Cys",
+    "NSR": "Asn",
+    "NYI": "Arg",
+    "AQV": "Ile",
+    "VDV": "Val",
+    "VNF": "Val",
+    "NLT": "Leu"
+}
+aminoacido_dict = {
+    "Ala": ["GCT", "GCC", "GCA", "GCG"],
+    "A": ["GCT", "GCC", "GCA", "GCG"],
+    "Arg": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],
+    "R": ["CGT", "CGC", "CGA", "CGG", "AGA", "AGG"],
+    "Asn": ["AAT", "AAC"],
+    "N": ["AAT", "AAC"],
+    "Asp": ["GAT", "GAC"],
+    "D": ["GAT", "GAC"],
+    "Cys": ["TGT", "TGC"],
+    "C": ["TGT", "TGC"],
+    "Gln": ["CAA", "CAG"],
+    "Q": ["CAA", "CAG"],
+    "Glu": ["GAA", "GAG"],
+    "E": ["GAA", "GAG"],
+    "Gin": ["CAA", "CAG"],
+    "Z": ["CAA", "CAG"],
+    "Gly": ["GGT", "GGC", "GGA", "GGG"],
+    "G": ["GGT", "GGC", "GGA", "GGG"],
+    "His": ["CAT", "CAC"],
+    "H": ["CAT", "CAC"],
+    "Ile": ["ATT", "ATC", "ATA"],
+    "I": ["ATT", "ATC", "ATA"],
+    "START": ["ATG"],
+    "lle": ["ATT", "ATC", "ATA"],
+    "Leu": ["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"],
+    "L": ["CTT", "CTC", "CTA", "CTG", "TTA", "TTG"],
+    "Lys": ["AAA", "AAG"],
+    "K": ["AAA", "AAG"],
+    "Met": ["ATG"],
+    "M": ["ATG"],
+    "Phe": ["TTT", "TTC"],
+    "F": ["TTT", "TTC"],
+    "Pro": ["CCT", "CCC", "CCA", "CCG"],
+    "P": ["CCT", "CCC", "CCA", "CCG"],
+    "Ser": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+    "S": ["TCT", "TCC", "TCA", "TCG", "AGT", "AGC"],
+    "Thr": ["ACT", "ACC", "ACA", "ACG"],
+    "T": ["ACT", "ACC", "ACA", "ACG"],
+    "Trp": ["TGG"],
+    "W": ["TGG"],
+    "Tyr": ["TAT", "TAC"],
+    "Y": ["TAT", "TAC"],
+    "Val": ["GTT", "GTC", "GTA", "GTG"],
+    "V": ["GTT", "GTC", "GTA", "GTG"],
+    "STOP": ["TAA", "TAG", "TGA"],
+}
+
+# La cadena de aminoácidos que deseas traducir
+cadena_aminoacidos = "MGYINVFAFPFTIYSLLLCRMNSRNYIAQVDVVNFNLT"
+
+# Función para traducir la cadena de aminoácidos a ARNm
+def traducir_a_ARNm(cadena_aminoacidos):
+    secuencia_arnm = ""
+    for i in range(0, len(cadena_aminoacidos), 3):
+        grupo = cadena_aminoacidos[i:i+3]
+        aminoacido = codon_dict.get(grupo, 'XXX')  # Obtener el aminoácido
+        if aminoacido != 'XXX':
+            codones = aminoacido_dict.get(aminoacido, [])
+            secuencia_arnm += "/".join(codones) + " "  # Agregar los codones del aminoácido
+        else:
+            secuencia_arnm += "XXX "  # Si no se encuentra el aminoácido, se agrega XXX
+    return secuencia_arnm.strip()
+
+# Llama a la función y muestra la secuencia de ARNm resultante
+secuencia_arnm = traducir_a_ARNm(cadena_aminoacidos)
+print(secuencia_arnm)
